b090bb700779e7310f43b5b98ea3a1820a497a31
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-10-22 21:23:11 +0300

    Add files via upload

---
+++ unit2_2.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+number_of_commands = int(input())
+stack = deque()
+for commands in range(number_of_commands):
+    numbers = input().split()
+    if int(numbers[0]) == 1:
+        number_for_deque = int(numbers[1])
+        stack.append(number_for_deque)
+    elif int(numbers[0]) == 2:
+        stack.popleft()
+    elif int(numbers[0]) == 3:
+        print(max(stack))
+    elif int(numbers[0]) == 4:
+        print(min(stack))
+    elif int(numbers[0]) == 5:
+        print(len(stack))
+stack.reverse()
+# print(*stack)
+print(", ".join(map(str, stack)))
