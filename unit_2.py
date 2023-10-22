b3ff6b2a98efccc97f49a388994b99cd17ed64e8
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-10-22 21:22:43 +0300

    Updated unit_2.py

--- unit_2.py
+++ unit_2.py
@@ -4,6 +4,7 @@ number_of_commands = int(input())
 stack = deque()
 for commands in range(number_of_commands):
     try:
+
         numbers = input().split()
         if int(numbers[0]) == 1:
             number_for_deque = int(numbers[1])
@@ -16,8 +17,21 @@ for commands in range(number_of_commands):
             print(min(stack))
         elif int(numbers[0]) == 5:
             print(len(stack))
+
     except IndexError:
-        if int(numbers[0]) == 2:
+        numbers = input().split()
+        if int(numbers[0]) == 1:
+            number_for_deque = int(numbers[1])
+            stack.append(number_for_deque)
+        elif int(numbers[0]) == 2:
             stack.popleft()
+        elif int(numbers[0]) == 3:
+            print(max(stack))
+        elif int(numbers[0]) == 4:
+            print(min(stack))
+        elif int(numbers[0]) == 5:
+            print(len(stack))
+
 stack.reverse()
-print(*stack)
+# print(*stack)
+print(", ".join(map(str, stack)))
