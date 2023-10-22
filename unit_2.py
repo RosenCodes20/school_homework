79ed132dfcbf0766d17bb03ef939bf0e62e6730d
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2023-10-22 20:36:22 +0300

    Updated unit_2.py

--- unit_2.py
+++ unit_2.py
@@ -8,6 +8,8 @@ for commands in range(number_of_commands):
         if int(numbers[0]) == 1:
             number_for_deque = int(numbers[1])
             stack.append(number_for_deque)
+        elif int(numbers[0]) == 2:
+            stack.popleft()
         elif int(numbers[0]) == 3:
             print(max(stack))
         elif int(numbers[0]) == 4:
